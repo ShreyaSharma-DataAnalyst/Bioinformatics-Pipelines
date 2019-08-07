8e5e779906bbb1426af92c6c5456f6cd93956bc1
Shreya Sharma <53847764+shreya-sharma20@users.noreply.github.com>
2019-08-07 12:20:57 +0530

    Add files via upload

---
+++ findKmers.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+
+#*************************Taking input from user*************************
+open(fh,"<$ARGV[0]");
+
+$i=0;
+
+while(<fh>){
+	chomp;
+	if($i == 0){	
+		$str=$_; 
+		$i++;
+	}
+	if($i == 1){
+		$mer=$_;
+	}	
+}
+
+
+#************Function calling to find kmers and printing them************
+@kmer=find_kmer(@str);
+print "kmers :\n"; 
+for($i=0;$i<$k;$i++){
+	print $i+1;print ". $kmer[$i]\n";
+	}
+
+#**********Function calling to find unique kmers and printing them*********
+
+@u_kmer=unique_kmer(@kmer);
+$i=0;
+print "Unique kmers :\n"; 
+foreach $y(@u_kmer){
+	print $i+1;
+	print ". $y\n";
+	$i++;
+	}
+
+#*************************Subroutine to find kmers*************************
+$k=0;
+$j=0;
+sub find_kmer(){
+	my @str=@_; #catching the array and making it local to subroutine
+	$len=length($str); 
+	$cnt=$len-$mer+1;
+	while($k<$cnt){
+		$kmer[$j]=substr $str, $k, $mer;
+		$j++;
+		$k++;
+	}
+	return @kmer
+}
+
+#***********************Subroutine to find unique kmers***********************
+
+sub unique_kmer(){
+	my @kmer=@_;  #catching the array and making it local to subroutine
+	for($i=0;$i<$cnt;$i++)  #setting counter to 0
+		{$count[$i]=0;}
+	$i=0;
+	$j=0;
+	$l=0;
+	while($i<$cnt){       #comparing 
+		for($j=0;$j<$i;$j++){
+			if($kmer[$i] eq $kmer[$j])
+				{$count[$i]++;break;}  
+			}
+		if($count[$i]==0)  #storing unique values in array @u_kmer
+			{$u_kmer[$l]=$kmer[$i];
+			$l++;}
+		$i++;
+	}
+	return @u_kmer;
+}
+
+
+
